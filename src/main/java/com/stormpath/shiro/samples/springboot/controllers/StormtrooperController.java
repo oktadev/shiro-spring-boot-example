571f9b43c88e934a39b2029635a20b880acb0fb4
Brian Demers <bdemers@apache.org>
2017-01-27 15:20:21 -0500

    Removing role annotations from controller

--- src/main/java/com/stormpath/shiro/samples/springboot/controllers/StormtrooperController.java
+++ src/main/java/com/stormpath/shiro/samples/springboot/controllers/StormtrooperController.java
@@ -2,9 +2,7 @@ package com.stormpath.shiro.samples.springboot.controllers;
 
 import com.stormpath.shiro.samples.springboot.common.dao.StormtrooperDao;
 import com.stormpath.shiro.samples.springboot.common.model.Stormtrooper;
-import org.apache.shiro.authz.annotation.Logical;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
-import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -33,14 +31,12 @@ public class StormtrooperController {
 
     @GetMapping()
     @RequiresPermissions("troopers:read")
-    @RequiresRoles(logical = Logical.OR, value = {"admin", "officer", "underling"})
     public Collection<Stormtrooper> listTroopers() {
         return trooperDao.listStormtroopers();
     }
 
     @GetMapping(path = "/{id}")
     @RequiresPermissions("troopers:read")
-    @RequiresRoles(logical = Logical.OR, value = {"admin", "officer", "underling"})
     public Stormtrooper getTrooper(@PathVariable("id") String id) throws NotFoundException {
 
         Stormtrooper stormtrooper = trooperDao.getStormtrooper(id);
@@ -52,7 +48,6 @@ public class StormtrooperController {
 
     @PostMapping()
     @RequiresPermissions("troopers:create")
-    @RequiresRoles(logical = Logical.OR, value = {"admin", "officer"})
     public Stormtrooper createTrooper(@RequestBody Stormtrooper trooper) {
 
         return trooperDao.addStormtrooper(trooper);
@@ -60,7 +55,6 @@ public class StormtrooperController {
 
     @PostMapping(path = "/{id}")
     @RequiresPermissions("troopers:update")
-    @RequiresRoles(logical = Logical.OR, value = {"admin", "officer"})
     public Stormtrooper updateTrooper(@PathVariable("id") String id, @RequestBody Stormtrooper updatedTrooper) throws NotFoundException {
 
         return trooperDao.updateStormtrooper(id, updatedTrooper);
@@ -70,7 +64,6 @@ public class StormtrooperController {
     @DeleteMapping(path = "/{id}")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     @RequiresPermissions("troopers:delete")
-    @RequiresRoles("admin")
     public void deleteTrooper(@PathVariable("id") String id) {
         trooperDao.deleteStormtrooper(id);
     }
